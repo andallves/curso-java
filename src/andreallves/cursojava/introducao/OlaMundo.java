b433e61e848bf3b7406d57ff5a4c89f09e0536f2
andallves <andrealves10a@gmail.com>
2023-08-23 20:20:49 -0300

    initial commit

---
+++ src/andreallves/cursojava/introducao/OlaMundo.java
@@ -0,0 +1,17 @@
+package andreallves.cursojava.introducao;
+
+/**
+ * This is a <b>javadoc</b> comments used for describe my class or another
+ * things.
+ */
+
+public class OlaMundo {
+    public static void main(String[] args) {
+        // Adding comments one line
+        /*
+            Comments in multiple lines
+        */
+        System.out.println("Hello world! I'm a dev Java");
+    }
+
+}
